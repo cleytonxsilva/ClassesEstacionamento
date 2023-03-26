3524a97fed4f5b65410f20c966db30d5f9d98108
Cleyton da Silva Pires <cleytonxsilva@outlook.com>
2023-03-26 19:53:46 -0300

    repositories

---
+++ src/main/java/br/com/uniamerica/estacionamento/repository/ModeloRepository.java
@@ -0,0 +1,10 @@
+package br.com.uniamerica.estacionamento.repository;
+
+import br.com.uniamerica.estacionamento.entity.Modelo;
+import org.springframework.boot.Banner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModeloRepository extends JpaRepository<Modelo, Long> {
+}
